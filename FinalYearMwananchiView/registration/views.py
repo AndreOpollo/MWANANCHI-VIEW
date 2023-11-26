2eaef2d3837a8fc81b1bf3ef9de4026ed4cea7fc
Ian kiprotich <113979508+onai254@users.noreply.github.com>
2023-11-26 09:51:47 +0300

    Updated testing folder

--- FinalYearMwananchiView/registration/views.py
+++ FinalYearMwananchiView/registration/views.py
@@ -4,6 +4,10 @@ from api.models import User
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from django.db import IntegrityError
+#from django.contrib.auth.tokens import password_reset_token
+#from django.core.mail import send_mail
+#from django.template.loader import render_to_string
+
 
 class UserRegisterView(APIView):
     def post(self, request):
@@ -17,5 +21,14 @@ class UserRegisterView(APIView):
         user = User(email=email,username = username)
         user.set_password(password)
         user.save()
+           # Generate activation token
+        #current_site = request.get_host()
+        #domain = 'http://' + current_site
+        #token = password_reset_token.generate_token(user)
+       # activation_url = f'{domain}/activate/{user.id}/{token}'
 
+        # Send activation email without HTML content
+        #subject = 'Activate Your Account'
+        #message = f"Please click the following link to activate your account: {activation_url}"
+        #send_mail(subject, message, 'no-reply@example.com', [user.email])
         return Response({'status': 'success'})
